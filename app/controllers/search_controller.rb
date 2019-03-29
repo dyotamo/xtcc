51472f88a4375573cd470562e291c688ec3f7559
Dássone <dyotamo@gmail.com>
2019-03-29 11:49:45 +0200

    Search...

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,13 @@
 class SearchController < ApplicationController
   def view
-    @text = params[:search][:text]
-    redirect_to root_path if @text.nil? || @text.empty?
-    @results = Mono.where("title like ?", "%#{@text}%").paginate(:page => params[:page], :per_page => 10)
+    @args = params[:search][:args]
+
+    if @args.nil? || @args.empty?
+      redirect_to root_path
+    end
+
+    @results = Mono.where("title like ? or author like ? or abstract like ?",
+                          "%#{@args}%", "%#{@args}%", "%#{@args}%")
+      .paginate(:page => params[:page], :per_page => 10)
   end
 end
