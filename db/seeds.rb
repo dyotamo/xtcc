f7d394e7c2f20b0ce586eee846049277dad12aec
Dássone <dyotamo@gmail.com>
2019-08-26 12:59:19 +0200

    Improving seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,14 @@
+require "csv"
 require "faker"
 
-r = Random.new
-
-20.times do
-  College.create(name: Faker::University.name)
+CSV.foreach(File.join(Rails.root, "db", "fixture", "colleges.csv"), headers: true) do |college|
+  College.create!(name: college["name"])
 end
 
-20.times do
-  Course.create(name: Faker::Book.publisher)
+CSV.foreach(File.join(Rails.root, "db", "fixture", "courses.csv"), headers: true) do |course|
+  Course.create!(name: course["name"])
 end
 
-20.times do
-  Requisition.create(url: Faker::Internet.url)
-end
-
-User.create({ email: "dyotamo@gmail.com",
-              password: "passwd",
-              password_confirmation: "passwd" })
-
-1000.times do
-  Mono.create!({ title: Faker::Book.title,
-               author: Faker::Book.author,
-               year: r.rand(2000..Time.now.year),
-               abstract: Faker::Lorem.paragraph,
-               url: Faker::Internet.url,
-               course_id: r.rand(1..20),
-               college_id: r.rand(1..20),
-               user_id: User.first.id })
-end
+User.create!({ email: "dyotamo@gmail.com",
+               password: "passwd",
+               password_confirmation: "passwd" })
